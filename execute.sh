12bedd638dfde7ca220a83a5d378ce113ba09e69
k1majd <keyvan.majd@gmail.com>
2022-02-20 22:24:09 -0700

    test update2

--- execute.sh
+++ execute.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
-# python examples/tc4_robot_control/net_train.py -ep 10 -nt 10
+python examples/tc4_robot_control/net_train.py -ep 10 -nt 10
 git add .
-git commit -m "test update"
+git commit -m "test update2"
 git push
