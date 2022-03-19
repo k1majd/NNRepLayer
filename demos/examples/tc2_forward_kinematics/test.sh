36ca945936762aeec3031fed520ec58d55b451de
k1majd <keyvan.majd@gmail.com>
2022-03-18 21:14:42 -0700

    Push FK repair log number 10

--- demos/examples/tc2_forward_kinematics/test.sh
+++ demos/examples/tc2_forward_kinematics/test.sh
@@ -5,7 +5,7 @@ for x in {1..30}
 do
   if (($x % 10 == 0))
   then
-    echo "$x"
+    echo "Push FK repair log timer $x"
     git pull origin emsoft_examples_replayer_update1
     git add .
     git commit -m "Push FK repair log number $x"
