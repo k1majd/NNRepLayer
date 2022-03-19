53c61f259dc10a81d55a38eedd27f32b41c9be5e
k1majd <keyvan.majd@gmail.com>
2022-03-18 21:09:30 -0700

    test update

---
+++ demos/examples/tc2_forward_kinematics/test.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+
+for x in {1..30}
+do
+  if (($x % 10 == 0))
+  then
+    echo "$x"
+    git pull origin emsoft_examples_replayer_update1
+    git add .
+    git commit -m "Push FK repair log number $x"
+    git push
+  fi
+  sleep 1
+done
+
