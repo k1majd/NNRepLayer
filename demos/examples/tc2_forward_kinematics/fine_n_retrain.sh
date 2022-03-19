28a7d131f078eb8b8ba207cff09a082bcdf35361
k1majd <keyvan.majd@gmail.com>
2022-03-18 21:10:01 -0700

    Push FK repair log number 10

--- demos/examples/tc2_forward_kinematics/fine_n_retrain.sh
+++ demos/examples/tc2_forward_kinematics/fine_n_retrain.sh
@@ -24,7 +24,7 @@ echo "Repair - finetune"
 for i in {2..3}
 do
    echo "repair_finetune - test: $i"
-   # python3 net_repair_finetune.py -rl $i -vi 0 -tl 86400 -mf 1
+   python3 net_repair_finetune.py -rl $i -vi 0 -tl 86400 -mf 1
    git pull origin emsoft_examples_replayer_update1
    git add .
    git commit -m "Update FK repair-finetune lay $i"
