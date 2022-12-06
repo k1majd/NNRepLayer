d8e82c059cfa35fb82641330c5c3361e04e45033
k1majd <54868379+k1majd@users.noreply.github.com>
2022-12-06 13:13:27 -0700

    example script for global constraint repair

--- examples/global_constraint/data/data_loader.py
+++ examples/global_constraint/data/data_loader.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import csv
+import matplotlib.pyplot as plt
 
 def loadData(name_csv):
     with open(name_csv) as csv_file:
@@ -53,28 +54,23 @@ def generateDataWindow(window_size=10):
 
 
 def generate_repair_dataset(num_samples, bound, model):
-    _,_, obs, ctrl = generateDataWindow(10)
+    _, _, obs, ctrl = generateDataWindow(10)
     ctrl_pred = model.predict(obs)
     max_window_size = 11000
-    delta_u = np.subtract(
-        ctrl_pred[0:max_window_size].flatten(),
-        obs[0:max_window_size, -1].flatten(),
-    )
+    obs = obs[0:max_window_size]
+    ctrl_pred = ctrl_pred[0:max_window_size].flatten()
+
     # violation_idx = np.argsort(np.abs(delta_u))[::-1]
-    violation_idx = np.where(np.abs(delta_u) > bound)[0]
-    temp = np.argsort(np.abs(delta_u[violation_idx]))[::-1]
+    violation_idx = np.where(ctrl_pred >= bound[1])[0]
+    temp = np.argsort(np.abs(ctrl_pred[violation_idx]))[::-1]
     violation_idx = violation_idx[temp]
-    nonviolation_idx = np.where(np.abs(delta_u) <= bound)[0]
+    nonviolation_idx = np.where(ctrl_pred < bound[1])[0]
     if violation_idx.shape[0] == 0:
         nonviolation_idx = np.random.choice(
             nonviolation_idx, size=num_samples, replace=False
         )
         return obs[nonviolation_idx], ctrl[nonviolation_idx]
     else:
-        # rnd_pts = np.random.choice(
-        #     int(violation_idx.shape[0]), int(num_samples * 0.75), replace=False
-        # )
-        # violation_idx = violation_idx[rnd_pts]
         violation_idx = np.random.choice(
             violation_idx, int(num_samples * 0.75), replace=False
         )
@@ -83,3 +79,35 @@ def generate_repair_dataset(num_samples, bound, model):
         )
         idx = np.concatenate((violation_idx, nonviolation_idx))
         return obs[idx], ctrl[idx]
+
+
+def plot_test_data(model_orig, model_repair, bounds):
+    _, _, obs, ctrl = generateDataWindow(10)
+    pred_ctrls_orig = model_orig(obs, training=False)
+    pred_ctrls_repair = model_repair(obs, training=False)
+
+    fig, (ax1, ax2) = plt.subplots(nrows=2)
+    ax1.plot(ctrl, color="#167fb8", label="Reference")
+    ax1.plot(pred_ctrls_orig, color="#1abd15", label="Original Predictions")
+    ax1.plot(
+        pred_ctrls_repair,
+        color="#b81662",
+        label="Repaired Predictions",
+    )
+    ax1.axhline(y=bounds[0], color="k", linestyle="dashed")  # upper bound
+    ax1.axhline(y=bounds[1], color="k", linestyle="dashed")  # lower bound
+    ax1.set_ylabel("Ankle Angle Control (rad)")
+    ax1.set_xlabel("Time (s)")
+    ax1.set_xlim([0, 500])
+    ax1.legend()
+
+    err_orig = np.abs(ctrl - pred_ctrls_orig)
+    err_repair = np.abs(ctrl - pred_ctrls_repair)
+    ax2.plot(err_orig, color="#1abd15")
+    ax2.plot(err_repair, color="#b81662")
+    ax2.grid(alpha=0.5, linestyle="dashed")
+    ax2.set_ylabel("Ankle Angle Control Error (rad)")
+    ax2.set_xlabel("Time (s)")
+    ax2.set_xlim([0, 500])
+
+    plt.show()
